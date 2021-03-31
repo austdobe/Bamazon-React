ea8da9963f00987c8ccc06ee76e3fa9873449352
Austin <austin.dober@gmail.com>
2021-03-30 21:37:42 -0400

    Removing unneeded files, adding React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homePage';
+import NoMatch from './pages/noMatch';
+import login from './pages/login'
+import history from './pages/history'
+
+const styling ={
+  App:{
+    minHeight: '100vh'
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={styling.App}>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/login' component={login} />
+          <Route exact path='/history' component={history}/>
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
